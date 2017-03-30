20ac007375df3550bf093e9c6ee6dbbf89c1f6c0
Jacqueline Gutman <Jacqueline.Gutman@nyumc.org>
2017-03-30 15:48:50 -0400

    create uniform table naming conventions, convert cols to dttm in R, timestamp in SQL, create code to read in all text files from directory into memory and reupload to database overwriting existing tables if necessary

--- rename_tables.sql
+++ rename_tables.sql
@@ -1,30 +1,59 @@
-RENAME TABLE edu_analytics.`2013_ALL_APPLICANTS` TO edu_analytics.`2013_all_applicants`;
-RENAME TABLE edu_analytics.`2013_MATRICULATED_REPORT` TO edu_analytics.`2013_matriculated_report`;
-RENAME TABLE edu_analytics.`2013_Tracking_Report_ALL_APPS` TO edu_analytics.`2013_tracking_report`;
+RENAME TABLE edu_analytics.`2013_all_applicants_orig` TO edu_analytics.`identified$raw$2013_all_applicants`;
+RENAME TABLE edu_analytics.`2013_matriculated_report_orig` TO edu_analytics.`identified$raw$2013_matriculated_report`;
+RENAME TABLE edu_analytics.`2013_tracking_report` TO edu_analytics.`identified$raw$2013_tracking_report`;
+RENAME TABLE edu_analytics.`2014_all_applicants_orig` TO edu_analytics.`identified$raw$2014_all_applicants`;
+RENAME TABLE edu_analytics.`2014_matriculated_report_orig` TO edu_analytics.`identified$raw$2014_matriculated_report`;
+RENAME TABLE edu_analytics.`2014_tracking_report` TO edu_analytics.`identified$raw$2014_tracking_report`;
+RENAME TABLE edu_analytics.`2015_all_applicants_orig` TO edu_analytics.`identified$raw$2015_all_applicants`;
+RENAME TABLE edu_analytics.`2015_matriculated_report_orig` TO edu_analytics.`identified$raw$2015_matriculated_report`;
+RENAME TABLE edu_analytics.`2015_tracking_report` TO edu_analytics.`identified$raw$2015_tracking_report`;
+RENAME TABLE edu_analytics.`2016_all_applicants_orig` TO edu_analytics.`identified$raw$2016_all_applicants`;
+RENAME TABLE edu_analytics.`2016_matriculated_report_orig` TO edu_analytics.`identified$raw$2016_matriculated_report`;
+RENAME TABLE edu_analytics.`2016_tracking_report` TO edu_analytics.`identified$raw$2016_tracking_report`;
 
-RENAME TABLE edu_analytics.`2014_ALL_APPLICANTS` TO edu_analytics.`2014_all_applicants`;
-RENAME TABLE edu_analytics.`2014_MATRICULATED_REPORT` TO edu_analytics.`2014_matriculated_report`;
-RENAME TABLE edu_analytics.`2014_TRACKING_REPORT` TO edu_analytics.`2014_tracking_report`;
+RENAME TABLE edu_analytics.`2013_all_applicants` TO edu_analytics.`hashed$raw$2013_all_applicants`;
+RENAME TABLE edu_analytics.`2013_matriculated_report` TO edu_analytics.`hashed$raw$2013_matriculated_report`;
+RENAME TABLE edu_analytics.`2014_all_applicants` TO edu_analytics.`hashed$raw$2014_all_applicants`;
+RENAME TABLE edu_analytics.`2014_matriculated_report` TO edu_analytics.`hashed$raw$2014_matriculated_report`;
+RENAME TABLE edu_analytics.`2015_all_applicants` TO edu_analytics.`hashed$raw$2015_all_applicants`;
+RENAME TABLE edu_analytics.`2015_matriculated_report` TO edu_analytics.`hashed$raw$2015_matriculated_report`;
+RENAME TABLE edu_analytics.`2016_all_applicants` TO edu_analytics.`hashed$raw$2016_all_applicants`;
+RENAME TABLE edu_analytics.`2016_matriculated_report` TO edu_analytics.`hashed$raw$2016_matriculated_report`;
 
-RENAME TABLE edu_analytics.`2015_ALL_APPLICANTS` TO edu_analytics.`2015_all_applicants`;
-RENAME TABLE edu_analytics.`2015_MATRICULATED_REPORT` TO edu_analytics.`2015_matriculated_report`;
-RENAME TABLE edu_analytics.`2015_TRACKING_REPORT` TO edu_analytics.`2015_tracking_report`;
+RENAME TABLE edu_analytics.ethnicity_raw_orig TO edu_analytics.`identified$raw$ethnicity`;
+RENAME TABLE edu_analytics.ethnicity_raw TO edu_analytics.`hashed$raw$ethnicity`;
+RENAME TABLE edu_analytics.ethnicity_orig TO edu_analytics.`identified$clean$ethnicity`;
+RENAME TABLE edu_analytics.ethnicity TO edu_analytics.`hashed$clean$ethnicity`;
 
-RENAME TABLE edu_analytics.`2015_ALL_APPLICANTS` TO edu_analytics.`2015_all_applicants`;
-RENAME TABLE edu_analytics.`2015_MATRICULATED_REPORT` TO edu_analytics.`2015_matriculated_report`;
-RENAME TABLE edu_analytics.`2015_TRACKING_REPORT` TO edu_analytics.`2015_tracking_report`;
+RENAME TABLE edu_analytics.gpa_scores_raw_orig TO edu_analytics.`identified$raw$gpa`;
+RENAME TABLE edu_analytics.gpa_scores_raw TO edu_analytics.`hashed$raw$gpa`;
+RENAME TABLE edu_analytics.gpa_orig TO edu_analytics.`identified$clean$gpa`;
+RENAME TABLE edu_analytics.gpa TO edu_analytics.`hashed$clean$gpa`;
 
-RENAME TABLE edu_analytics.`2016_ALL_APPLICANTS` TO edu_analytics.`2016_all_applicants`;
-RENAME TABLE edu_analytics.`2016_Matriculated_Report` TO edu_analytics.`2016_matriculated_report`;
-RENAME TABLE edu_analytics.`2016_Tracking_Report` TO edu_analytics.`2016_tracking_report`;
+RENAME TABLE edu_analytics.mcat_raw_orig TO edu_analytics.`identified$raw$old_mcat`;
+RENAME TABLE edu_analytics.mcat_raw TO edu_analytics.`hashed$raw$old_mcat`;
+RENAME TABLE edu_analytics.old_mcat_scores_orig TO edu_analytics.`identified$clean$old_mcat`;
+RENAME TABLE edu_analytics.old_mcat_scores TO edu_analytics.`hashed$clean$old_mcat`;
 
-RENAME TABLE edu_analytics.Parent_Guardian TO edu_analytics.parent_guardian;
+RENAME TABLE edu_analytics.new_mcat_raw_orig TO edu_analytics.`identified$raw$new_mcat`;
+RENAME TABLE edu_analytics.new_mcat_raw TO edu_analytics.`hashed$raw$new_mcat`;
+RENAME TABLE edu_analytics.new_mcat_scores_orig TO edu_analytics.`identified$clean$new_mcat`;
+RENAME TABLE edu_analytics.new_mcat_scores TO edu_analytics.`hashed$clean$new_mcat`;
 
-RENAME TABLE edu_analytics.ETHNICITY_RAW_ORIG TO edu_analytics.ethnicity_raw_orig;
-RENAME TABLE edu_analytics.GPA_SCORES_RAW_ORIG TO edu_analytics.gpa_scores_raw_orig;
-RENAME TABLE edu_analytics.MCAT_RAW_ORIG TO edu_analytics.mcat_raw_orig;
-RENAME TABLE edu_analytics.RACE_RAW_ORIG TO edu_analytics.race_raw_orig;
-RENAME TABLE edu_analytics.SCHOOL_RAW_ORIG TO edu_analytics.school_raw_orig;
-RENAME TABLE edu_analytics.MMI_SCORES_ADMISSION_RAW_ORIG TO edu_analytics.mmi_scores_raw_orig;
-RENAME TABLE edu_analytics.NEW_MCAT_RAW_ORIG TO edu_analytics.new_mcat_raw_orig;
-RENAME TABLE edu_analytics.mmi_scores_admission_raw TO edu_analytics.mmi_scores_raw;
+RENAME TABLE edu_analytics.mmi_scores_raw_orig TO edu_analytics.`identified$raw$mmi_scores`;
+RENAME TABLE edu_analytics.`raw$mmi_scores` TO edu_analytics.`hashed$raw$mmi_scores`;
+RENAME TABLE edu_analytics.mmi_scores_orig TO edu_analytics.`identified$clean$mmi_scores`;
+RENAME TABLE edu_analytics.mmi_scores TO edu_analytics.`hashed$clean$mmi_scores`;
+
+RENAME TABLE edu_analytics.parent_guardian_orig TO edu_analytics.`identified$raw$parent_guardian`;
+RENAME TABLE edu_analytics.parent_guardian TO edu_analytics.`hashed$raw$parent_guardian`;
+
+RENAME TABLE edu_analytics.race_raw_orig TO edu_analytics.`identified$raw$race`;
+RENAME TABLE edu_analytics.race_raw TO edu_analytics.`hashed$raw$race`;
+RENAME TABLE edu_analytics.race_orig TO edu_analytics.`identified$clean$race`;
+RENAME TABLE edu_analytics.race TO edu_analytics.`hashed$clean$race`;
+
+RENAME TABLE edu_analytics.school_raw_orig TO edu_analytics.`identified$raw$school`;
+RENAME TABLE edu_analytics.school_raw TO edu_analytics.`hashed$raw$school`;
+RENAME TABLE edu_analytics.schools_orig TO edu_analytics.`identified$clean$school`;
+RENAME TABLE edu_analytics.schools TO edu_analytics.`hashed$clean$school`;
