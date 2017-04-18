5c4d0e1710afb68622d1938fb9c32e88540b568a
Jacqueline Gutman <Jacqueline.Gutman@nyumc.org>
2017-04-18 14:29:15 -0400

    all notebooks updated with fns up to including prepare_for_cleaning_tbls.Rmd, table_naming_schema updated to reflect all tables in database

--- dbUtilities/R/tbl_functions.R
+++ dbUtilities/R/tbl_functions.R
@@ -160,7 +160,9 @@ add_tbl_prefix <- function(tbl_names,
 #' @export
 #'
 write_to_database_single <- function(df, conn, tbl_name) {
-  dbWriteTable(conn, tbl_name, df,
+  df %>%
+  mutate_if(is.logical, as.integer) %>%
+  dbWriteTable(conn, tbl_name, .,
                     row.names = FALSE, overwrite = TRUE)
 }
 
