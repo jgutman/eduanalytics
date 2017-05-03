1589ada4f419c5be62818a4ede49b8339a10c0cd
Jacqueline Gutman <Jacqueline.Gutman@nyumc.org>
2017-05-03 13:30:22 -0400

    update table names and add supp_info table, begin work on binary indicator processing function and screening exploration and descriptives

---
+++ setup_notebook_dana.R
@@ -0,0 +1,32 @@
+## ---- setup_notebook
+library(RMySQL)
+library(yaml)
+library(data.table)
+library(knitr)
+library(tidyverse)
+library(stringr)
+library(magrittr)
+library(lubridate)
+
+##  devtools::install("dbUtilities",
+#    dependencies = FALSE, quiet = TRUE)
+library(dbUtilities)
+
+knitr::opts_chunk$set(
+    message = FALSE,
+    warning = FALSE,
+    comment = "   ",
+    cache = TRUE,
+    autodep = TRUE,
+    cache.comments = FALSE)
+
+# credentials_path <- "/Volumes/IIME/EDS/data/admissions/db_credentials"
+credentials_path <- "/Users/iime/Desktop/admissions"
+reconnect <- function() {
+    get_mysql_conn(path = credentials_path,
+        credentials_file = "edu_deid.my.cnf",
+                   group = "edu_deident") %>%
+    assign("edu_deid_con", ., envir = .GlobalEnv)
+}
+
+reconnect()
