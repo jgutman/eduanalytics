94a23848390535df3e6682bf6724a27256d357ef
Jacqueline Gutman <jacqueline.gutman@nyumc.org>
2017-08-29 16:21:42 -0400

    simplify conda environment edu_analytics_simple

--- run_simple.sh
+++ run_simple.sh
@@ -2,7 +2,7 @@
 dir="${HOME}/Desktop/admissions_supp"
 today=`date +%m_%d_%y`
 cd $HOME/Desktop/admissions
-source activate edu_analytics
+source activate edu_analytics_simple
 python run_and_save_model.py --predict --id 13 23 --pkldir ${dir}/pkls \
 --dyaml all_features_non_urm.yaml all_features_urm.yaml \
 --credpath ${dir}/db_credentials &> ${dir}/logs/log_${today}.out
